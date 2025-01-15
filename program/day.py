d73dee7530ddab6124b2e9df7074185b313bbfe8
danbaguley <danbaguley@outlook.com>
2025-01-15 16:33:18 +0000

    Created day and task classes

---
+++ program/day.py
@@ -0,0 +1,5 @@
+class Day:
+    def __init__(self, name, date):
+        self.name = name
+        self.date = date
+        self.tasks = []
---
+++ program/task.py
@@ -0,0 +1,5 @@
+class Task:
+    def __init__(self, name, description, completed):
+        self.name = name
+        self.description = description
+        self.completed = completed
